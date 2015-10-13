82173af48f1eb479ab0f8a96fcddf89c655e4c0b
Miguel Rafael Esteban Martín <miguel.esteban@logicaalternativa.com>
2015-10-13 07:02:39 +0200

    it's added license

--- src/test/java/com/logicaalternativa/examples/akka/bus/PublishSimpleSuscribeTest.java
+++ src/test/java/com/logicaalternativa/examples/akka/bus/PublishSimpleSuscribeTest.java
@@ -1,3 +1,24 @@
+/*
+ *      PublishSimpleSuscribeTest.java
+ *      
+ *      Copyright 2016 Miguel Rafael Esteban Martín (www.logicaalternativa.com) <miguel.esteban@logicaalternativa.com>
+ *      
+ *      This program is free software; you can redistribute it and/or modify
+ *      it under the terms of the GNU General Public License as published by
+ *      the Free Software Foundation; either version 2 of the License, or
+ *      (at your option) any later version.
+ *      
+ *      This program is distributed in the hope that it will be useful,
+ *      but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *      MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *      GNU General Public License for more details.
+ *      
+ *      You should have received a copy of the GNU General Public License
+ *      along with this program; if not, write to the Free Software
+ *      Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston,
+ *      MA 02110-1301, USA.
+ */
+
 package com.logicaalternativa.examples.akka.bus;
 
 import static org.junit.Assert.assertEquals;
@@ -51,7 +72,7 @@ public class PublishSimpleSuscribeTest extends TestBase {
 		
 		__INFO("Only for waiting all agent results ");
 		
-		Thread.sleep( ( System.currentTimeMillis() - now ) * 10  );
+		Thread.sleep( ( System.currentTimeMillis() - now ) * 12  );
 		
 		
 		__INFO("... and finally, it is going to check all the futur result");
